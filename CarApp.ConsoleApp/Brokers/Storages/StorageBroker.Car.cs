41f0707bec3f50d709b25fd76dc8b6e470b98079
Lavanya Juturu (Wipro Ltd.) <v-lajutu@microsoft.com>
2022-05-06 14:49:25 -0700

    Broker: Insert Car

---
+++ CarApp.ConsoleApp/Brokers/Storages/StorageBroker.Car.cs
@@ -0,0 +1,20 @@
+﻿// ------------------------------------------------
+// Copyright (c) MumsWhoCode. All rights reserved.
+// ------------------------------------------------
+
+using CarApp.ConsoleApp.Models.Cars;
+
+namespace CarApp.ConsoleApp.Brokers.Storages
+{
+    internal partial class StorageBroker : IStorageBroker
+    {
+        List<Car> Cars = new List<Car>();
+
+        public Car InsertCar(Car car)
+        {
+            Cars.Add(car);
+
+            return car;
+        }
+    }
+}
