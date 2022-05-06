2626d1ee6504c704857aa240a1f7e1b7b26d3076
Lavanya Juturu (Wipro Ltd.) <v-lajutu@microsoft.com>
2022-05-06 11:25:48 -0700

    INFRA: Initialize Unit Test

--- CarApp.ConsoleApp/Program.cs
+++ CarApp.ConsoleApp/Program.cs
@@ -2,8 +2,6 @@
 // Copyright (c) MumsWhoCode. All rights reserved.
 // ------------------------------------------------
 
-using System;
-
 namespace CarApp.ConsoleApp
 {
     internal class Program
